394a4833514a016120830355d7edc751bb9d87f8
TheSilentOccupant <TheSilentOccupant@users.noreply.github.com>
2023-04-06 13:31:32 -0700

    player templates made

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,15 @@
+public enum Size
+{
+    Small,
+    Medium,
+    Large,
+}
+
+public enum Chance
+{
+    Miss,
+    Low,
+    Medium,
+    High,
+    Guaranteed,
+}
--- Assets/Scripts/PlayerTemplate.cs
+++ Assets/Scripts/PlayerTemplate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Character", menuName = "Character Data", order = 51)]
+[CreateAssetMenu(fileName = "NewCharacter", menuName = "Character/Data", order = 51)]
 public class PlayerTemplate : ScriptableObject
 {
     [SerializeField]
@@ -10,9 +10,41 @@ public class PlayerTemplate : ScriptableObject
     [SerializeField]
     private string _description;
     [SerializeField]
-    private int goldCost;
+    private int _health;
     [SerializeField]
-    private int attackDamage;
+    private int _armor;
+    [SerializeField]
+    private int _shotStrengthMin;
+    [SerializeField]
+    private int _shotStrengthMax;
+    [SerializeField]
+    private float _shotSpeed;
+    [SerializeField]
+    private Size _shotSize;
+    [SerializeField]
+    private int _shotMonsterMin;
+    [SerializeField]
+    private int _shotMonsterMax;
+    [SerializeField]
+    private int _shotGeneratorMin;
+    [SerializeField]
+    private int _shotGeneratorMax;
+    [SerializeField]
+    private int _shotPotionMonsterMin;
+    [SerializeField]
+    private int _shotPotionMonsterMax;
+    [SerializeField]
+    private int _shotPotionGeneratorMin;
+    [SerializeField]
+    private int _shotPotionGeneratorMax;
+    [SerializeField]
+    private int _meleeMonsterMin;
+    [SerializeField]
+    private int _meleeMonsterMax;
+    [SerializeField]
+    private Chance _meleeGeneratorChance;
+    [SerializeField]
+    private int _speed;
 
     public string CharacterName
     {
@@ -23,13 +55,93 @@ public class PlayerTemplate : ScriptableObject
         get { return _description; }
     }
 
-    public int GoldCost
+    public int Health
     {
-        get { return goldCost; }
+        get { return _health; }
     }
 
-    public int AttackDamage
+    public int Armor
     {
-        get { return attackDamage; }
+        get { return _armor; }
+    }
+
+    public int ShotStrengthMin
+    {
+        get { return _shotStrengthMin; }
+    }
+
+    public int ShotStrengthMax
+    {
+        get { return _shotStrengthMax; }
+    }
+
+    public float ShotSpeed
+    {
+        get { return _shotSpeed; }
+    }
+
+    public Size ShotSize
+    {
+        get { return _shotSize; }
+    }
+
+    public int ShotMonsterMin
+    {
+        get { return _shotMonsterMin; }
+    }
+
+    public int ShotMonsterMax
+    {
+        get { return _shotMonsterMax; }
+    }
+
+    public int ShotGeneratorMin
+    {
+        get { return _shotGeneratorMin; }
+    }
+
+    public int ShotGeneratorMax
+    {
+        get { return _shotGeneratorMax; }
+    }
+
+    public int ShotPotionMonsterMax
+    {
+        get { return _shotPotionMonsterMax; }
+    }
+
+    public int ShotPotionMonsterMin
+    {
+        get { return _shotPotionMonsterMin; }
+    }
+
+    public int ShotPotionGeneratorMin
+    {
+        get { return _shotPotionGeneratorMin; }
+    }
+
+    public int ShotPotionGeneratorMax
+    {
+        get { return _shotPotionGeneratorMax; }
+    }
+
+    public int MeleeMonsterMin
+    {
+        get { return _meleeMonsterMin; }
+    }
+
+    public int MeleeMonsterMax
+    {
+        get { return _meleeMonsterMax; }
+    }
+
+    public Chance MeleeGeneratorChance
+    {
+        get { return _meleeGeneratorChance; }
+    }
+
+    public int Speed
+    {
+        get { return _speed; }
     }
 }
