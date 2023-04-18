13f3d105049783199b02b252975b937094ea4316
Westenwurt <114536550+Alyssawurt@users.noreply.github.com>
2023-04-18 13:21:59 -0700

    Added Lobber Enemy, Normal Enemy, and Spawner
    Added that stuff

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,32 +5,46 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    private int Health;
+    private int _health;
 
-    private Vector3 location;
+    private Vector3 _location;
 
-    private GameObject Player;
+    protected private GameObject _player;
 
-    private NavMeshAgent Agent;
+    protected private NavMeshAgent _agent;
+
+    public int health { get { return _health; } set { value = _health; } }
 
     // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.Find("Player");
-        Agent = this.GetComponent<NavMeshAgent>();
+        _player = GameObject.Find("Player");
+        _agent = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        Agent.destination = Player.transform.position;
+        _agent.destination = _player.transform.position;
     }
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage;
+    }
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Player Hit Owie");
+            //other.gameobject.GetComponent<Player>.TakeDamage();
+            this.gameObject.SetActive(false);
+        }
+        if (other.gameObject.tag == "Bullet")
+        {
+            TakeDamage(5);
         }
     }
 }
